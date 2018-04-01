c77e0e9e871d0a0808e7a7a4f8917d56df322198
Leonardo Setubal Maggio <setubal43@gmail.com>
2018-04-01 16:24:32 -0300

    Primeiras Class e Alteração no Settings

--- Concessionaria/Carros/models.py
+++ Concessionaria/Carros/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Proprietario(models.Model):
+    SEXO_CHOICES = (
+        ("feminino", "Feminino"),
+        ("masculino", "Masculino"),
+    )
+    nome = models.CharField(max_length=50, null=False)
+    data_nascimento = models.DateField(null=False, verbose_name="Data de Nascimento")
+    cpf = models.CharField(max_length=20, null=False)
+    sexo = models.CharField(max_length=20, null=False, choices=SEXO_CHOICES)
+    profissao = models.CharField(max_length=20)
+    telefone = models.CharField(max_length=20, null=False)
+
+    class Meta:
+        verbose_name_plural = "Proprietarios"
+
+    def __str__(self):
+        return self.nome
