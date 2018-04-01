20fa90e87956427e4fc23c1199482821effe43cf
Leonardo Setubal Maggio <setubal43@gmail.com>
2018-04-01 16:27:59 -0300

    Restante das classes

--- Concessionaria/Carros/models.py
+++ Concessionaria/Carros/models.py
@@ -18,3 +18,44 @@ class Proprietario(models.Model):
 
     def __str__(self):
         return self.nome
+
+ class Acessorio(models.Model):
+       ESTADO_CHOICES = (
+           ("ótimo", "Ótimo"),
+           ("bom", "Bom"),
+          ("ruim", "Ruim"),
+       )
+       descricao = models.CharField(max_length=50, null=False)
+       estado = models.CharField(max_length=20, choices=ESTADO_CHOICES)
+       def __str__(self):
+           return self.descricao
+
+
+class Veiculo(models.Model):
+    CORES_CHOICES = (
+        ("preto", "Preto"),
+        ("azul", "Azul"),
+        ("amarelo", "Amarelo"),
+        ("branco", "Branco"),
+        ("prata", "Prata"),
+        ("vermelho", "Vermelho"),
+    )
+    TIPO_CHOICES = (
+        ("moto", "Moto"),
+        ("carro", "Carro"),
+    )
+    modelo = models.CharField(max_length=50, null=False)
+    marca = models.CharField(max_length=20, null=False)
+    placa = models.CharField(max_length=8, null=False)
+    cor = models.CharField(max_length=20, null=False, choices=CORES_CHOICES)
+    ano = models.IntegerField(null=False)
+    preco = models.FloatField(null=False)
+    foto_capa = models.ImageField(upload_to='images')
+    tipo = models.CharField(max_length=10, choices=TIPO_CHOICES)
+
+    proprietario = models.ForeignKey(Proprietario)
+    acessorios = models.ManyToManyField(Acessorio)
+
+
+    def __str__(self):
+        return self.modelo
