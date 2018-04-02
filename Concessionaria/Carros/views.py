992ee110fed668ada3c9eebf66d9c734c345e120
Leonardo Setubal Maggio <setubal43@gmail.com>
2018-04-02 17:36:00 -0300

    Teste

--- Concessionaria/Carros/views.py
+++ Concessionaria/Carros/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from .models import Veiculo
+from django.shortcuts import render
+from django.core import paginator
+from django.core.paginator import PageNotAnInteger,EmptyPage,Paginator
 
 
 # Create your views here.
@@ -10,15 +12,51 @@ from .models import Veiculo
 #     return render(request, 'veiculos_list.html', veiculos)
 
 # listar veiculos por pesquisa
+
+# def listar_veiculo(request):
+#     query = request.GET.get("busca", '')
+#     if query:
+#         veiculo = Veiculo.objects.filter(modelo__icontains=query)
+#     else:
+#       veiculo = Veiculo.objects.all()
+#     veiculos = {'lista': veiculo}
+#     return render(request, 'veiculos_list.html', veiculos)
+
+# Organizar
+# def listar_veiculo(request):
+#     query = request.GET.get("busca", '')
+#     ordenar = request.GET.get("ordenar", '')
+#
+#     if query:
+#         veiculo = Veiculo.objects.filter(modelo__icontains=query)
+#     else:
+#
+#         if ordenar:
+#             veiculo = Veiculo.objects.all().order_by(ordenar)
+#
+#         else:
+#             veiculo = Veiculo.objects.all()
+#     veiculos = {'lista': veiculo}
+#     return render(request, 'veiculos_list.html', veiculos)
+
+#Paginacao
 def listar_veiculo(request):
-    query = request.GET.get("busca", '')
-    if query:
-        veiculo = Veiculo.objects.filter(modelo__icontains=query)
-    else:
-      veiculo = Veiculo.objects.all()
-    veiculos = {'lista': veiculo}
-    return render(request, 'veiculos_list.html', veiculos)
-
-dasdasd
-
-asda
+     query = request.GET.get("busca", '')
+     page = request.GET.get('page', '')
+     ordenar = request.GET.get("ordenar", '')
+     if query:
+         veiculo = Veiculo.objects.filter(modelo__icontains=query)
+     else:
+         try:
+             if ordenar:
+                 veiculo = Veiculo.objects.all().order_by(ordenar)
+             else:
+                 veiculo = Veiculo.objects.all()
+             veiculo = Paginator(veiculo, 1)
+             veiculo = veiculo.page(page)
+         except PageNotAnInteger:
+             veiculo = veiculo.page(1)
+         except EmptyPage:
+             veiculo = paginator.page(paginator.num_pages)
+     veiculos = {'lista': veiculo}
+     return render(request, 'veiculos_list.html', veiculos)
