a932d8b05fa3fe54e658214d20c817ce15c20593
Leonardo Setubal Maggio <setubal43@gmail.com>
2018-04-01 22:43:38 -0300

    erro na url

--- Concessionaria/Carros/views.py
+++ Concessionaria/Carros/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Proprietario,Veiculo,Acessorio
 # Create your views here.
+def listar_veiculo(request, template_name="veiculos_list.html"):
+   veiculo = Veiculo.objects.all()
+   veiculos = {'lista': veiculo}
+   return render(request, template_name, veiculos)
