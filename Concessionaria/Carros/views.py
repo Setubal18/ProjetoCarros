52cbc865f45da2731e6052cbc9e52c84fb35b5d1
Leonardo Setubal Maggio <setubal43@gmail.com>
2018-04-02 15:49:11 -0300

    Listagem por pesquisa e inicio de ordenar

--- Concessionaria/Carros/views.py
+++ Concessionaria/Carros/views.py
@@ -3,7 +3,18 @@ from .models import Veiculo
 
 
 # Create your views here.
-def listar_veiculo(request, ):
-    veiculo = Veiculo.objects.all()
+# Listar todos os veiculos
+# def listar_veiculo(request):
+#     veiculo = Veiculo.objects.all()
+#     veiculos = {'lista': veiculo}
+#     return render(request, 'veiculos_list.html', veiculos)
+
+# listar veiculos por pesquisa
+def listar_veiculo(request):
+    query = request.GET.get("busca", '')
+    if query:
+        veiculo = Veiculo.objects.filter(modelo__icontains=query)
+    else:
+      veiculo = Veiculo.objects.all()
     veiculos = {'lista': veiculo}
     return render(request, 'veiculos_list.html', veiculos)
