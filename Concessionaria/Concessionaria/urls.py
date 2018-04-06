a7570670a3751f8b6c73d6180f5674aa160659f5
Leonardo Setubal Maggio <setubal43@gmail.com>
2018-04-06 15:39:27 -0300

    HTLM Concertado e com path arrumandinho porra que dlc!!

--- Concessionaria/Concessionaria/urls.py
+++ Concessionaria/Concessionaria/urls.py
@@ -17,7 +17,7 @@ from Carros.views import listar_veiculo
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path
 
 urlpatterns = [
                   path("veiculo/lista/", listar_veiculo, name="listar_veiculo"),
